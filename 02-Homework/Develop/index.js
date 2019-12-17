878fe684f0c0f1eb12a2a8317cfeea66fdc56d23
Johan Bueno <johan.s.bueno@gmail.com>
2019-12-17 18:20:51 -0500

    html working

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -14,45 +14,49 @@ function gitHubUser() {
         },
 
         {
-            type: "checkbox",
+            type: "list",
             choices: ["red", "pink", "blue", "green"],
             name: "color",
 
         },
 
     ])
-        .then(function ({ username }) {
-            const queryUrl = "https://api.github.com/users/" + username
+        .then(function ({ username, color }) {
+            const queryUrl = `https://api.github.com/users/${username}`
 
-            axios.get(queryUrl).then(function (res) {
+           return axios.get(queryUrl).then(function (res) {
                 //  const general = res.data
-                const gitHubName = res.data.name
-                const userLink = res.data.repos_url
-                const imgUser = res.data.avatar_url
-                const bio = res.data.bio
-                const company = res.data.company
-                const publiR = res.data.public_repos
-                const followers = res.data.followers
-                const following = res.data.following
-                const location = res.data.location
+                // const gitHubName = res.data.name
+                // const userLink = res.data.repos_url
+                // const imgUser = res.data.avatar_url
+                // const bio = res.data.bio
+                // const company = res.data.company
+                // const publiR = res.data.public_repos
+                // const followers = res.data.followers
+                // const following = res.data.following
+                // const location = res.data.location
 
-                console.log(gitHubName)
-                console.log(imgUser)
-                console.log(bio)
-                console.log(userLink);
-                console.log(company)
-                console.log(publiR)
-                console.log(followers)
-                console.log(following)
-                console.log(location)
+                // console.log(gitHubName)
+                // console.log(imgUser)
+                // console.log(bio)
+                // console.log(userLink);
+                // console.log(company)
+                // console.log(publiR)
+                // console.log(followers)
+                // console.log(following)
+                // console.log(location)
                 //  console.log(general);
-
-
+                console.log(color,"<=======")
+                fs.writeFile("test.html",generateHTML(res, color),(err)=>{
+                    if(err) throw err
+                    console.log("wrote the file")
+                })
             })
         })
 }
 
-function generateHTML(data) {
+function generateHTML(res, color) {
+    //console.log(res);
 
     return `<!DOCTYPE html>
   <html lang="en">
@@ -68,41 +72,44 @@ function generateHTML(data) {
     </head>
     <body>
     <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-        <div class="row">
-      ////////
-           <div class = "wrapper">
-            <h1 class="photo-header"> ${data.username}</h1>
+     <div class="container">
+      <div class="row">
+
+        <div class = "wrapper">
+            <h1 class="photo-header" style = "color: ${color}"> ${res.data.name}</h1>
             <hr>
-            <img class="photo-header img" src="${data.color}" alt="" style="width: 250px;height: 250px;">
+            <img class="photo-header img" src="${res.data.avatar_url}" alt="" style="width: 250px;height: 250px;">
             <div class="card" > </div>
-            <div> Company:</div>
-            <div> Repo</div>
-            <div> Public</div>
-            <div> Follo</div>
-            <div>Location</div>
-            </div>
-         </div>
-
-         </div>
+            <div> Company:${res.data.company}</div>
+            <a src= "${res.data.public_repos}"> Repo Link </a>
+            <div> Public Repositories: ${res.data.public_repos}</div>
+            <div> Followers: ${res.data.followers}</div>
+            <div> Following: ${res.data.following}</div>
+            <div>Location: ${res.data.location} </div>
+        </div>
+       </div>
+      </div>
     </div>
-    ////////
 
-  </body>   
+</body>
 
-  </html>`;
+</html>`;
 
 }
 
-gitHubUser()
-    .then(function (data) {
-        const html = generateHTML(data);
-        return writeFileAsync('index.html', html);
-    })
-    .then(function () {
-        console.log("Success");
-    })
-    .catch(function (err) {
+async function init(){
+try { 
+    const res = await gitHubUser();
+
+    // const html = generateHTML(res);
+    // await  writeFileAsync('index.html', html);
+    } 
+    catch(err) {
         console.log(err);
-    });
+    };
+
+}
+  
+init();
+    
 
